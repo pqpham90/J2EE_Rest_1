cc8ee5b865121d2ce55ba630cc92098890fda232
PhillipQuang Pham <pqpham90@gmail.com>
2015-04-05 21:09:22 -0500

    starting part 2

--- src/main/java/assign/resources/EavesdropResource.java
+++ src/main/java/assign/resources/EavesdropResource.java
@@ -1,19 +1,10 @@
 package assign.resources;
 
-import assign.domain.Course;
-import assign.domain.Courses;
 import assign.domain.Project;
 import assign.domain.Projects;
 import assign.services.EavesdropService;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.StreamingOutput;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -21,11 +12,9 @@ import javax.xml.bind.Marshaller;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.ListIterator;
 
-@Path("/myeavesdrop")
+@Path("/myeavesdrop/projects")
 public class EavesdropResource {
-
 	EavesdropService eavesdropService;
 
 	public EavesdropResource() {
@@ -33,130 +22,40 @@ public class EavesdropResource {
 	}
 
 	@GET
-	@Path("/helloworld")
-	@Produces("text/html")
-	public String helloWorld() {
-		return "Hello world";
-	}
-
-	@GET
-	@Path("/helloeavesdrop")
-	@Produces("text/html")
-	public String helloEavesdrop() {
-		return this.eavesdropService.getData();
-	}
-
-	@GET
-	@Path("/courses")
-	@Produces("application/xml")
-	public StreamingOutput getAllCourses() throws Exception {
-		Course modernWebApps = new Course();
-		modernWebApps.setDepartment("CS");
-		modernWebApps.setName("Modern Web Applications");
-
-		Course operatingSystems = new Course();
-		operatingSystems.setDepartment("CS");
-		operatingSystems.setName("Operating Systems");
-
-		final Courses courses = new Courses();
-		courses.setEmployees(new ArrayList<Course>());
-		courses.getCourses().add(modernWebApps);
-		courses.getCourses().add(operatingSystems);
-
-		return new StreamingOutput() {
-			public void write(OutputStream outputStream) throws IOException, WebApplicationException {
-				outputCourses(outputStream, courses);
-			}
-		};
-	}
-
-	@GET
-	@Path("/projects")
+	@Path("/")
 	@Produces("application/xml")
 	public StreamingOutput getAllProjects() throws Exception {
-		String baseURL = "http://eavesdrop.openstack.org/";
-		String irclogs = "irclogs/";
-		String meetings = "meetings/";
-
 		final Projects projects = new Projects();
 		projects.setProjects(new ArrayList<String>());
 
-		try {
-			Document doc = Jsoup.connect(baseURL + irclogs).get();
-			Elements links = doc.select("body a");
-
-			ListIterator<Element> iter = links.listIterator();
-			while(iter.hasNext()) {
-				Element e = iter.next();
-				String s = e.html();
-				if (s.endsWith("/")) {
-					s = s.replace("/", "");
-					projects.getProjects().add(s);
-				}
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		try {
-			Document doc = Jsoup.connect(baseURL + meetings).get();
-			Elements links = doc.select("body a");
-
-			ListIterator<Element> iter = links.listIterator();
-			while(iter.hasNext()) {
-				Element e = iter.next();
-				String s = e.html();
-				if (s.endsWith("/")) {
-					s = s.replace("/", "");
-					projects.getProjects().add(s);
-				}
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-//		Project heat = new Project();
-//		heat.setName("%23heat");
+		eavesdropService.getAllProjects(projects);
 
 		return new StreamingOutput() {
 			public void write(OutputStream outputStream) throws IOException, WebApplicationException {
-				outputCourses(outputStream, projects);
+				outputProjects(outputStream, projects);
 			}
 		};
 	}
 
 	@GET
-	@Path("/project")
+	@Path("{project}/irclogs")
 	@Produces("application/xml")
-	public StreamingOutput getProject() throws Exception {
-		final Project heat = new Project();
-		heat.setName("%23heat");
-		heat.setLink(new ArrayList<String>());
-		heat.getLink().add("l1");
-		heat.getLink().add("l2");
+	public StreamingOutput getProject(@PathParam("project") String projectName) throws Exception {
+		final Project project = new Project();
+		project.setName("%23heat");
+		project.setLink(new ArrayList<String>());
+		project.getLink().add("l1");
+		project.getLink().add("l2");
 
 		return new StreamingOutput() {
 			public void write(OutputStream outputStream) throws IOException, WebApplicationException {
-				outputCourses(outputStream, heat);
+				outputProject(outputStream, project);
 			}
 		};
 	}
 
-	protected void outputCourses(OutputStream os, Courses courses) throws IOException {
-		try {
-			JAXBContext jaxbContext = JAXBContext.newInstance(Courses.class);
-			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			jaxbMarshaller.marshal(courses, os);
-		} catch (JAXBException jaxb) {
-			throw new WebApplicationException();
-		}
-	}
-
-	protected void outputCourses(OutputStream os, Projects projects) throws IOException {
+	protected void outputProjects(OutputStream os, Projects projects) throws IOException {
 		try {
 			JAXBContext jaxbContext = JAXBContext.newInstance(Projects.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
@@ -169,7 +68,7 @@ public class EavesdropResource {
 		}
 	}
 
-	protected void outputCourses(OutputStream os, Project project) throws IOException {
+	protected void outputProject(OutputStream os, Project project) throws IOException {
 		try {
 			JAXBContext jaxbContext = JAXBContext.newInstance(Project.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
--- src/main/java/assign/services/EavesdropService.java
+++ src/main/java/assign/services/EavesdropService.java
@@ -1,9 +1,60 @@
 package assign.services;
 
+import assign.domain.Projects;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ListIterator;
+
 public class EavesdropService {
 
 	public String getData() {
 		return "Hello from Eavesdrop service.";
 	}
-	
+
+	public void getAllProjects(Projects projects) throws Exception {
+		String baseURL = "http://eavesdrop.openstack.org/";
+		String irclogs = "irclogs/";
+		String meetings = "meetings/";
+
+		try {
+			Document doc = Jsoup.connect(baseURL + meetings).get();
+			Elements links = doc.select("body a");
+
+			ListIterator<Element> iter = links.listIterator();
+			while(iter.hasNext()) {
+				Element e = iter.next();
+				String s = e.html();
+				if (s.endsWith("/")) {
+					s = s.replace("/", "");
+					projects.getProjects().add(s);
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Document doc = Jsoup.connect(baseURL + irclogs).get();
+			Elements links = doc.select("body a");
+
+			ListIterator<Element> iter = links.listIterator();
+			while(iter.hasNext()) {
+				Element e = iter.next();
+				String s = e.html();
+				if (s.endsWith("/")) {
+					s = s.replace("/", "");
+					s = s.replace("#", "%23");
+					projects.getProjects().add(s);
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
